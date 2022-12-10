3293a90d611eac212e06d4b4cfaf5903a8e73416
Anıl Bayar <75955104+bayaranil@users.noreply.github.com>
2022-12-10 23:14:38 +0300

    Welcome to Lessons

--- Img_Read_Show/Img_Read_Show.py
+++ Img_Read_Show/Img_Read_Show.py
@@ -1,7 +1,7 @@
 import cv2
 img = cv2.imread("inovatim.jpeg",0) #görsel okuma ,#0 =grayscale
 #print(img)
-cv2.namedWindow("image",cv2.WINDOW_NORMAL) # pencere boyutlandırması
+cv2.namedWindow("image", cv2.WINDOW_NORMAL) # pencere boyutlandırması
 cv2.imshow("image",img) # görseli gösterme
 cv2.imwrite("D:\inovatim\inovatim_2_Gray_Scale.jpeg",img) # görseli uzantıya kaydetme
 cv2.waitKey(0) # bekleme time (ms)
